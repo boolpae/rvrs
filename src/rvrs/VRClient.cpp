b2c0a0486628ffe64882907d79ffe90250d61c5c
이승훈(잇팩) <boolpae@itfact.co.kr>
2018-03-26 16:23:36 +0900

    VRClient, 문자열 비교 로직 추가27

--- src/rvrs/VRClient.cpp
+++ src/rvrs/VRClient.cpp
@@ -185,7 +185,7 @@ void VRClient::thrdMain(VRClient* client) {
                             }
                             #endif
                             for(sttIdx=0; sttIdx<srcLen; sttIdx++) {
-                                if ((dstBuff[srcLen-sttIdx]<0xa0)  && !memcmp(srcBuff, dstBuff, srcLen-sttIdx)) {
+                                if (!memcmp(srcBuff, dstBuff, srcLen-sttIdx) && (dstBuff[srcLen-sttIdx] < 0xa0)) {
                                     break;
                                 }
                             }
