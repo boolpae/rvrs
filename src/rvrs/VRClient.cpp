db9f333ca05fc116f7f7781410b6cf7321be9466
이승훈(잇팩) <boolpae@itfact.co.kr>
2018-03-26 10:27:25 +0900

    VRClient, 문자열 비교 로직 추가10

--- src/rvrs/VRClient.cpp
+++ src/rvrs/VRClient.cpp
@@ -106,6 +106,11 @@ void VRClient::thrdMain(VRClient* client) {
 
 	// m_cJobType에 따라 작업 형태를 달리해야 한다. 
 	if (client->m_cJobType == 'R') {
+#if 1
+        uint32_t sttIdx;
+        string tmpStt;
+        const char*tmpSttString;
+#endif
         uint32_t diaNumber=1;   // DB 실시간 STT 테이블에 저장될 호(Call)단위 Index 값
 		// 실시간의 경우 통화가 종료되기 전까지 Queue에서 입력 데이터를 받아 처리
 		// FILE인 경우 기존과 동일하게 filename을 전달하는 방법 이용
@@ -158,7 +163,32 @@ void VRClient::thrdMain(VRClient* client) {
                             //client->m_Logger->debug("VRClient::thrdMain(%s) - start_pos(%lu), end_pos(%lu).", client->m_sCallId.c_str(), start, end);
                             *pEndpos = 0;
                         }
+#if 1                        
                         client->m_Logger->debug("VRClient::thrdMain(%s) - pEndpos(%s).", client->m_sCallId.c_str(), pEndpos);
+                        pEndpos = (char *)value;
+                        
+                        if (tmpStt.size() < strlen(pEndpos)) {
+                            sttIdx = 0;
+                            tmpSttString = tmpStt.c_str();
+                            for(sttIdx=0; sttIdx<tmpStt.size(); sttIdx++) {
+                                if (memcmp(tmpSttString+sttIdx, pEndpos+sttIdx, sizeof(char))) {
+                                    break;
+                                }
+                            }
+                            pEndpos = pEndpos + sttIdx;
+                        }
+                        tmpStt = (char *)value;
+
+                        // to DB
+                        if (client->m_r2d) {
+                            client->m_r2d->insertRtSTTData(diaNumber, client->m_sCallId, item->spkNo, pEndpos ? start : vPos[item->spkNo -1].bpos/160, pEndpos ? end : vPos[item->spkNo -1].epos/160, std::string((const char*)pEndpos));
+                        }
+                        //STTDeliver::instance(client->m_Logger)->insertSTT(client->m_sCallId, std::string((const char*)value), item->spkNo, vPos[item->spkNo -1].bpos, vPos[item->spkNo -1].epos);
+                        // to STTDeliver(file)
+                        if (client->m_deliver) {
+                            client->m_deliver->insertSTT(client->m_sCallId, std::string((const char*)pEndpos), item->spkNo, pEndpos ? start : vPos[item->spkNo -1].bpos/160, pEndpos ? end : vPos[item->spkNo -1].epos/160);
+                        }
+#else
                         // to DB
                         if (client->m_r2d) {
                             client->m_r2d->insertRtSTTData(diaNumber, client->m_sCallId, item->spkNo, pEndpos ? start : vPos[item->spkNo -1].bpos/160, pEndpos ? end : vPos[item->spkNo -1].epos/160, std::string((const char*)value));
@@ -168,6 +198,7 @@ void VRClient::thrdMain(VRClient* client) {
                         if (client->m_deliver) {
                             client->m_deliver->insertSTT(client->m_sCallId, std::string((const char*)value), item->spkNo, pEndpos ? start : vPos[item->spkNo -1].bpos/160, pEndpos ? end : vPos[item->spkNo -1].epos/160);
                         }
+#endif
                         free(value);
                         
                         diaNumber++;
