3124c05caa8a82802f417840cf5057e538913997
이승훈(잇팩) <boolpae@itfact.co.kr>
2018-03-26 09:14:52 +0900

    VRClient, 문자열 비교 로직 추가

--- src/rvrs/VRClient.cpp
+++ src/rvrs/VRClient.cpp
@@ -106,6 +106,9 @@ void VRClient::thrdMain(VRClient* client) {
 
 	// m_cJobType에 따라 작업 형태를 달리해야 한다. 
 	if (client->m_cJobType == 'R') {
+        const char* tmpSttString;
+        uint32_t sttIdx;
+        string tmpStt="";
         uint32_t diaNumber=1;   // DB 실시간 STT 테이블에 저장될 호(Call)단위 Index 값
 		// 실시간의 경우 통화가 종료되기 전까지 Queue에서 입력 데이터를 받아 처리
 		// FILE인 경우 기존과 동일하게 filename을 전달하는 방법 이용
@@ -158,6 +161,20 @@ void VRClient::thrdMain(VRClient* client) {
                             //client->m_Logger->debug("VRClient::thrdMain(%s) - start_pos(%lu), end_pos(%lu).", client->m_sCallId.c_str(), start, end);
                             *pEndpos = 0;
                         }
+                        
+                        // 문자열 비교 로직
+                        sttIdx=0;
+                        tmpSttString = tmpStt.c_str();
+                        if (tmpStt.length() < strlen(pEndpos)) {
+                            for (sttIdx=0; sttIdx<tmpStt.length(); sttIdx++) {
+                                if (memcmp(tmpSttString+sttIdx, pEndpos+sttIdx, sizeof(char))) {
+                                    break;
+                                }
+                            }
+                        }
+                        pEndpos = (char *)(tmpSttString + sttIdx);
+                        tmpStt = (char *)value;
+                        
                         // to DB
                         if (client->m_r2d) {
                             client->m_r2d->insertRtSTTData(diaNumber, client->m_sCallId, item->spkNo, pEndpos ? start : vPos[item->spkNo -1].bpos/160, pEndpos ? end : vPos[item->spkNo -1].epos/160, std::string((const char*)value));
